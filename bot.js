cf143150a6afa7aed047cc27c6b8bec33446f0f3
Yulong Tan <yulongtan67@gmail.com>
2018-02-07 17:53:05 -0800

    adds initial functions

--- bot.js
+++ bot.js
@@ -18,22 +18,37 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
+    const desc = 'I should definitely be studying rather than making a Discord bot...';
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
+    const prefix = '!';
+
+    // Don't do anything there's no message
+    if (!message.startsWith(prefix)) return;
+    if (message.startsWith(prefix)) {
+        const args = message.slice(prefix.length).trim().split(/ +/g);
+        var command = args.shift().toLowerCase();
+        if (command == 'deets') {
+            bot.sendMessage({
+                to: channelID,
+                message: `User: ${user}\nUser ID: ${userID}\nChannel ID: ${channelID}\nDescription: ${desc}`
+            });
+        } else if (command == 'cl') {
+            bot.sendMessage({
+                to: channelID,
+                message: '```JavaScript' + '\nconsole.log(' + args.toString() + ')```'
+            })
+        } else if (command == 'roles') {
+            bot.sendMessage({
+                to: channelID,
+                message: 'Brandon: Healer\nSunny: DPS\nYulong: Tank\nCharlie: all me guy'
+            })
+        } else if (command == 'asl') {
+            let [age, sex, location] = args;
+            bot.sendMessage({
+                to: userID,
+                message: `Hello ${user.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna skate?`
+            })
+        }
      }
 });
